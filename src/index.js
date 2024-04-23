8fd62f86bb79baacadb876acb77cc4e7e3fa4fc4
Alex H <alex.holder@bigfootds.com>
2024-04-23 20:34:55 +1000

    feat: promise file checker is done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 
 const fs = require("node:fs");
+const fsPromises = require("node:fs/promises");
+
 
 console.log("Promise output:" + doesFileExist("./pokemonStats.json"));
 console.log("Sync output:" + doesFileExistSync("./pokemonStats.json"));
@@ -8,13 +10,28 @@ console.log("Sync output:" + doesFileExistSync("./pokemonStats.json"));
 function doesFileExist(targetPath){
 	let result = false;
 
+	return new Promise((resolve, reject) => {
+		fsPromises.stat(targetPath).then(statData => {
+			if (statData.size || statData.birthtimeMs) {
+				result = true;
+				console.log(result);
+				resolve(result)
+			} else {
+				resolve(result)
+			}
+		}).catch(error => {
+			reject(error);
+		});
 
+	});
+
+	
 	// function(data, data, whatever, callbackFunction)
 	// fs.exists(targetPath, doSomethingWithResult());
-	fs.exists(targetPath, (existsResult) => {
-		result = existsResult;
-		return result;
-	});
+	// fs.exists(targetPath, (existsResult) => {
+	// 	result = existsResult;
+	// 	return result;
+	// });
 
 	// fs.exists(targetPath).then((existsResult) => {
 	// 	// if (existsResult){
